cb02322d5ff45c79e6c39f3e5c970ef046a58dde
Mikhail Zinchenko <zinchenko@surfstudio.co>
2020-04-02 14:56:32 +0300

    refactor main activities

--- app/src/main/java/com/liarstudio/moviesearch/ui/detail/MovieDetailActivity.kt
+++ app/src/main/java/com/liarstudio/moviesearch/ui/detail/MovieDetailActivity.kt
@@ -1,11 +1,11 @@
 package com.liarstudio.moviesearch.ui.detail
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.liarstudio.moviesearch.R
 import com.liarstudio.moviesearch.domain.Movie
-import com.liarstudio.moviesearch.model.repo.MovieRepositoryProvider
 import kotlinx.android.synthetic.main.activity_movie_detail.*
 
 class MovieDetailActivity : AppCompatActivity() {
@@ -13,21 +13,25 @@ class MovieDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
+        showMovie(intent.getSerializableExtra(MOVIE_EXTRA) as Movie)
+    }
 
-        val movie = intent.getSerializableExtra(MOVIE_EXTRA) as Movie
+    private fun showMovie(movie: Movie) {
+        movie_title_tv.text = movie.title
 
         Glide.with(movie_poster_iv)
             .load("https://image.tmdb.org/t/p/w370_and_h556_bestv2" + movie.posterUrl)
             .into(movie_poster_iv)
 
-        movie_title_tv.text = movie.title
+        findSame(movie)
+    }
 
-        val repository = MovieRepositoryProvider.movieRepository
-        repository.findSame(movie, { list ->
-            movie_same_name_1.text = list[0].title
-            movie_same_name_2.text = list[1].title
-            movie_same_name_3.text = list[2].title
-        })
+    private fun findSame(movie: Movie) {
+        //TODO поиск похожих фильмов
+    }
+
+    private fun showError() {
+        Toast.makeText(this, "Ошибка при загрузке", Toast.LENGTH_SHORT).show()
     }
 
     companion object {
--- app/src/main/java/com/liarstudio/moviesearch/ui/main/MainActivity.kt
+++ app/src/main/java/com/liarstudio/moviesearch/ui/main/MainActivity.kt
@@ -6,33 +6,28 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.liarstudio.moviesearch.ui.detail.MovieDetailActivity
 import com.liarstudio.moviesearch.R
 import com.liarstudio.moviesearch.domain.Movie
+import com.liarstudio.moviesearch.model.API_KEY
+import com.liarstudio.moviesearch.model.BASE_URL
+import com.liarstudio.moviesearch.model.api.MovieApi
+import com.liarstudio.moviesearch.model.response.MovieListResponse
 import com.liarstudio.moviesearch.ui.main.controller.MovieListController
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import ru.surfstudio.android.easyadapter.EasyAdapter
 import ru.surfstudio.android.easyadapter.ItemList
 
 class MainActivity : AppCompatActivity() {
 
     val adapter = EasyAdapter()
-    val controller =
-        MovieListController {
-            startActivity(
-                Intent(
-                    this,
-                    MovieDetailActivity::class.java
-                ).putExtra(
-                    MovieDetailActivity.MOVIE_EXTRA,
-                    it
-                )
-            )
-        }
-
-    val viewModel = MainVM()
+    val controller = MovieListController(onMovieClick = { openDetailsScreen(it) })
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,15 +35,50 @@ class MainActivity : AppCompatActivity() {
 
         movies_rv.adapter = adapter
         movies_rv.layoutManager = LinearLayoutManager(this)
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
 
-        viewModel.movies.observe(this, Observer { result ->
-            when {
-                result.isSuccess -> showMovies(result.getOrThrow())
-                result.isFailure -> showError()
+        val movieApi = retrofit.create(MovieApi::class.java)
+        val call = movieApi.discover(
+            API_KEY,
+            "RU"
+        )
+
+        call.enqueue(object : Callback<MovieListResponse> {
+            override fun onFailure(call: Call<MovieListResponse>, t: Throwable) {
+                showError()
+            }
+
+            override fun onResponse(
+                call: Call<MovieListResponse>,
+                response: Response<MovieListResponse>
+            ) {
+                if (response.isSuccessful && response.body() != null) {
+                    val list = response.body()?.convert() ?: emptyList()
+                    showMovies(list)
+                }
             }
         })
 
-        search_et.addTextChangedListener(createOnTextChangeListener())
+
+//        search_et.addTextChangedListener(createOnTextChangeListener()) //TODO поиск текста
+    }
+
+    private fun createOnTextChangeListener() = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            //ничего не делаем
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            //ничего не делаем
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val text = s ?: return
+            //TODO поиск текста
+        }
     }
 
     private fun showError() {
@@ -59,22 +89,13 @@ class MainActivity : AppCompatActivity() {
         ).show()
     }
 
-    fun createOnTextChangeListener() = object : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {
-
-        }
-
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-        }
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            val text = s ?: return
-            viewModel.searchText.value = text.toString()
-        }
-    }
-
     fun showMovies(movies: List<Movie>) {
         adapter.setItems(ItemList.create().addAll(movies, controller))
+    }
 
+    private fun openDetailsScreen(movie: Movie) {
+        val intent = Intent(this, MovieDetailActivity::class.java)
+        intent.putExtra(MovieDetailActivity.MOVIE_EXTRA, movie)
+        startActivity(intent)
     }
 }
